6ed3d9f8b6b7c33f7e50cef5e875e2c72b9132cf
nath-asha <omthasha@gmail.com>
2024-06-05 13:53:13 +0530

    ll

--- backend/index.js
+++ backend/index.js
@@ -1,69 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
 const cors = require('cors');
+const movieRoutes = require('./routes/movieRoutes');
+const { connectDB } = require('./config/db');
+
+dotenv.config();
 
 const app = express();
-const PORT = 5000;
-
-// Middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
-// MongoDB connection
-mongoose.connect('mongodb://localhost:27017/movieCatalogue', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+connectDB();
 
-const movieSchema = new mongoose.Schema({
-    title: String,
-    director: String,
-    genre: String,
-    year: Number,
-});
+app.use('/api/movies', movieRoutes);
 
-const Movie = mongoose.model('Movie', movieSchema);
-
-// CRUD routes
-app.post('/movies', async (req, res) => {
-    const movie = new Movie(req.body);
-    await movie.save();
-    res.send(movie);
-});
-
-app.get('/movies', async (req, res) => {
-    const { genre } = req.query;
-    const filter = genre ? { genre } : {};
-    const movies = await Movie.find(filter);
-    res.send(movies);
-});
-
-app.get('/movies/:id', async (req, res) => {
-    const movie = await Movie.findById(req.params.id);
-    res.send(movie);
-});
-
-app.put('/movies/:id', async (req, res) => {
-    const movie = await Movie.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.send(movie);
-});
-
-app.delete('/movies/:id', async (req, res) => {
-    await Movie.findByIdAndDelete(req.params.id);
-    res.send({ message: 'Movie deleted' });
-});
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
-
-// const express = require('express');
-// const app = express();
-
-// app.get('/',(req,res)=>{
-//     res.send('Hello');
-// });
-// app.listen(3000, ()=> {
-//     console.log('Server listening on port 3000');
-// });
