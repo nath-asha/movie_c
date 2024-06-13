5f3e6953a9351c19e452117e71f0627a8eb293e3
nath-asha <omthasha@gmail.com>
2024-06-13 16:17:59 +0530

    mm

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Welcome to Movie Catalogue</h1>
-        </div>
-    );
+  return (
+    <div className="container">
+      <h1>Welcome to the Movie Catalogue</h1>
+      <p>Find and explore your favorite movies.</p>
+    </div>
+  );
 };
 
 export default Home;
