d367d5bc207d327b60423f086ae0d678db240088
nath-asha <omthasha@gmail.com>
2024-06-13 16:14:02 +0530

    mm

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useContext } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Register from './pages/Register';
@@ -18,12 +18,12 @@ const App = () => {
         <AuthProvider>
             <Router>
                 <Navbar />
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route path="/register" component={Register} />
-                    <Route path="/login" component={Login} />
-                    <Route path="/movies" component={Movies} />
-                </Switch>
+                <Routes>
+                    <Route exact path="/" element={Home} />
+                    <Route path="/register" element={Register} />
+                    <Route path="/login" element={Login} />
+                    <Route path="/movies" element={Movies} />
+                </Routes>
             </Router>
         </AuthProvider>
     );
